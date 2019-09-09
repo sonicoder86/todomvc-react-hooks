09424c948eee9d01518fce2515aeed2663d19d04
blacksonic <soos.gabor86@gmail.com>
2019-09-09 20:08:05 +0200

    feat(layout): create initial layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import 'todomvc-app-css/index.css';
+import { App } from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <div>
+    <App />
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by <a href="http://github.com/blacksonic/">blacksonic</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  </div>,
+  document.getElementById('root')
+);
