7ae1ddca5208b6467388911518dea9e9b65ebff8
blacksonic <soos.gabor86@gmail.com>
2019-09-13 19:55:39 +0200

    feat(header): implement events

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,13 @@ import { List } from './List';
 import { Footer } from './Footer';
 
 export function App() {
+  const onCreate = (name) => {
+    console.log('new todo', name);
+  };
+
   return (
     <section className="todoapp">
-      <Header />
+      <Header onSave={onCreate} />
       <List />
       <Footer />
     </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export function Header() {
+const ENTER_KEY = 13;
+
+export function Header({ onSave }) {
+  const [name, setName] = useState('');
+
+  const handleChange = event => setName(event.target.value);
+
+  const handleSubmit = event => {
+    if (event.which === ENTER_KEY) {
+      onSave(name);
+      setName('');
+    }
+  };
+
   return (
     <header className="header">
       <h1>todos</h1>
-      <input className="new-todo" placeholder="What needs to be done?" value="" />
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={name}
+        onChange={handleChange}
+        onKeyDown={handleSubmit}
+      />
     </header>
   );
 }
+
+Header.propTypes = {
+  onSave: PropTypes.func.isRequired
+};
