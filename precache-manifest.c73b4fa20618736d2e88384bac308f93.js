885bfc7f9935867af49959a7825163af36a0db12
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 11:22:08 +0000

    Deploy blacksonic/todomvc-react-hooks to github.com/blacksonic/todomvc-react-hooks.git:gh-pages

---
+++ precache-manifest.c73b4fa20618736d2e88384bac308f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df625115370c947c76e000025bdffbfb",
+    "url": "https://blacksonic.github.io/todomvc-react/index.html"
+  },
+  {
+    "revision": "1266ea3d10e3069672bb",
+    "url": "https://blacksonic.github.io/todomvc-react/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "1266ea3d10e3069672bb",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/2.3bd0e42d.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/2.3bd0e42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0cd2f319bcbb62e358",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/main.f15599d4.chunk.js"
+  },
+  {
+    "revision": "9fe02577a04a49cfeee8",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/runtime-main.d63c6c5a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://blacksonic.github.io/todomvc-react/precache-manifest.c73b4fa20618736d2e88384bac308f93.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blacksonic.github.io/todomvc-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
