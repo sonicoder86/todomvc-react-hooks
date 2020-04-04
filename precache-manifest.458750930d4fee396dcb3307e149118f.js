38e92de441f8164d70d7e1df5bd7ae5bb4deaf3f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 15:51:15 +0000

    Deploy blacksonic/todomvc-react-hooks to github.com/blacksonic/todomvc-react-hooks.git:gh-pages

---
+++ precache-manifest.458750930d4fee396dcb3307e149118f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048dea7bb745d2d00851cdd729f5bb6f",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/index.html"
+  },
+  {
+    "revision": "1266ea3d10e3069672bb",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "1266ea3d10e3069672bb",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/static/js/2.3bd0e42d.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/static/js/2.3bd0e42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0cd2f319bcbb62e358",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/static/js/main.f15599d4.chunk.js"
+  },
+  {
+    "revision": "369d05c54cfa5a125395",
+    "url": "https://blacksonic.github.io/todomvc-react-hooks/static/js/runtime-main.27a53bb9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://blacksonic.github.io/todomvc-react/precache-manifest.c73b4fa20618736d2e88384bac308f93.js"
+  "https://blacksonic.github.io/todomvc-react-hooks/precache-manifest.458750930d4fee396dcb3307e149118f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blacksonic.github.io/todomvc-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blacksonic.github.io/todomvc-react-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
